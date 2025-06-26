2bb1e75dc0fda5a6f6f3975e5d69759762afcbe1
Macarron <marianacifuenteszu@gmail.com>
2025-06-26 00:09:23 -0500

    wip backlog & historia + editar

---
+++ js/smooth-scroll.js
@@ -0,0 +1,13 @@
+// Scroll suave personalizado para toda la página
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
